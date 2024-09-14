85bbe68da8388c953af561ca18f5848d6b511992
J0EL0E <144648977+J0EL0E@users.noreply.github.com>
2024-09-14 21:23:05 +0800

    remove option in connectDB

--- server.js
+++ server.js
@@ -22,10 +22,8 @@ const port = process.env.API_SERVER_URL || 3000;
 
 async function connectDB () {
     try{
-        await mongoose.connect( process.env.DB_URI  , {
-            serverSelectionTimeoutMS: 10000 
-    });
-        console.log(`MongoDB Connected`);
+        await mongoose.connect( process.env.DB_URI , () => {
+            console.log(`MongoDB Connected`);
     }catch(err){
         console.log(err)
     }
